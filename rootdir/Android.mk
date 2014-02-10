fbb4fce115a2a69eef5f9fe3efa66aec29cd8e76
pio <piomasaki@gmail.com>
2014-02-09 20:10:57 -0700

    Change names/labels to dlxub1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,10 +2,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= fstab.dlx
+LOCAL_MODULE		:= fstab.dlxub1ub1
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/fstab.dlx
+LOCAL_SRC_FILES		:= etc/fstab.dlxub1ub1
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -18,33 +18,33 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.dlx.usb.rc
+LOCAL_MODULE		:= init.dlxub1.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.dlx.usb.rc
+LOCAL_SRC_FILES		:= etc/init.dlxub1.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.dlx.rc
+LOCAL_MODULE		:= init.dlxub1.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.dlx.rc
+LOCAL_SRC_FILES		:= etc/init.dlxub1.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= ueventd.dlx.rc
+LOCAL_MODULE		:= ueventd.dlxub1.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/ueventd.dlx.rc
+LOCAL_SRC_FILES		:= etc/ueventd.dlxub1.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.dlx.rc
+LOCAL_MODULE       := init.recovery.dlxub1.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.dlx.rc
+LOCAL_SRC_FILES    := etc/init.recovery.dlxub1.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
